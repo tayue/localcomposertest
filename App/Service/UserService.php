53e6417c8dd3afbaa1ba16e7e110dd96cae23c5d
tayue <denghui@imyfone.cn>
2021-01-13 09:49:16 +0800

    订单

---
+++ App/Service/UserService.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Framework\SwServer\Pool\DiPool;
+
+use Framework\SwServer\Rpc\Annotation\Mapping\RpcService;
+
+
+class UserService
+{
+    public function notify($orderData)
+    {
+        print_r($orderData);
+        echo __CLASS__ . "/" . __METHOD__ . "\r\n";
+    }
+
+    public function findUser()
+    {
+
+        $userData = [];
+         
+        return $userData;
+    }
+
+
+    public function getList(int $uid, string $type){
+        return [$uid,$type];
+    }
+}
